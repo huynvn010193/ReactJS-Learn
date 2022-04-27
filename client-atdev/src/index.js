9ebddb8ad97231e026a3c8b668670881bfd422f5
huynvn <Huy.Nguyen@navigosgroup.com>
2022-04-27 17:12:38 +0700

    React Query Tutorial #01

--- client-atdev/src/index.js
+++ client-atdev/src/index.js
@@ -6,16 +6,23 @@ import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import  { ContextProvider } from './context/store'
+import  { ContextProvider } from './context/store';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-axios.defaults.baseURL = 'http://localhost:5200/api'
+axios.defaults.baseURL = 'http://localhost:5200/api';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ContextProvider>
-        <App />
-        <ToastContainer />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ToastContainer />
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
+        </QueryClientProvider>
       </ContextProvider>
     </BrowserRouter>
    </React.StrictMode>,
