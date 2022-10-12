88cf03686247856a41ceee9b4015fca6a1d4b7f5
huynvn <Huy.Nguyen@navigosgroup.com>
2022-10-13 00:16:55 +0700

    Pending 49:16 clip 3

--- GrapQL-Apolo-Crash/client/src/components/BookDetails.js
+++ GrapQL-Apolo-Crash/client/src/components/BookDetails.js
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Card from 'react-bootstrap/Card'
+import { useQuery } from '@apollo/client';
+import { getSingleBook } from '../graphql-client/queries';
 
-const BookDetails = () => {
+const BookDetails = ({ bookId }) => {
+  const { loading, error, data } = useQuery(getSingleBook, {
+    variables: { 
+      id: bookId
+    }
+  });
+
+  if(loading) return <p>Loading book details...</p>;
+  if(bookId !== null && error) {
+    return <p>Error loading book details!</p>
+  }
+
+  const book = !loading && !error ? data.book : null;
+
+
   return (
     <Card bg="info" text="white" className="shadow">
       <Card.Body>
-        <Card.Title>Ky nghe lay tay</Card.Title>
-        <Card.Subtitle>Ky nghe lay tay</Card.Subtitle>
-        <Card.Text>Vu Trong Phung</Card.Text>
-        <Card.Text>Age: 99</Card.Text>
-        <Card.Text>All book by this author</Card.Text>
-        <ul>
-          <li>Ky nghe lay tay</li>
-          <li>So do</li>
-        </ul> 
+        {
+          book === null ? ( <Card.Text>Please select a book</Card.Text> ) : (
+            <Fragment>
+              <Card.Title>{book.name}</Card.Title>
+              <Card.Subtitle>{book.genre}</Card.Subtitle>
+              <p>{book.author.name}</p>
+              <p>Age: {book.author.age}</p>
+              <p>All book by this author</p>
+              <ul>
+                {book.author.books.map(
+                  item => <li key={item.id}>{item.name}</li>
+                )}
+                <li>Ky nghe lay tay</li>
+                <li>So do</li>
+              </ul>
+            </Fragment>
+          ) 
+        }
       </Card.Body>
     </Card>
   )
