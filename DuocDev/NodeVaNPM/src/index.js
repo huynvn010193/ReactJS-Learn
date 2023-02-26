3d1d2936f3ca6df6ed3d7b5493ab1c6a3e6c5f11
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-26 15:50:48 +0700

    17.Webpack and webpack CLI

---
+++ DuocDev/NodeVaNPM/src/index.js
@@ -0,0 +1,3 @@
+import sum from './utils';
+
+console.log(sum(10, 50));
---
+++ DuocDev/NodeVaNPM/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+console.log('__dirname', __dirname);
+console.log('path.resolve()', path.resolve());
+console.log('path.resolve(__dirname, dist)', path.resolve(__dirname, 'dist'));
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.resolve('src/index.js'), // quy định tên file build xong
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.s[ac]ss|css$/,
+  //       use: ['style-loader', 'css-loader', 'sass-loader'],
+  //     },
+  //   ],
+  // },
+};
