28c8a0435907012c20a0a696c3d51cb3ecd06ac1
huynvn <Huy.Nguyen@navigosgroup.com>
2023-02-26 17:00:11 +0700

    18.Xu ly Loader va CSS trong webpack

--- DuocDev/NodeVaNPM/webpack.config.js
+++ DuocDev/NodeVaNPM/webpack.config.js
@@ -1,21 +1,23 @@
 const path = require('path');
-console.log('__dirname', __dirname);
-console.log('path.resolve()', path.resolve());
-console.log('path.resolve(__dirname, dist)', path.resolve(__dirname, 'dist'));
+
+// console.log('__dirname', __dirname);
+// console.log('path.resolve()', path.resolve());
+// console.log('path.resolve(__dirname, dist)', path.resolve(__dirname, 'dist'));
+
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     app: path.resolve('src/index.js'), // quy định tên file build xong
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.s[ac]ss|css$/,
-  //       use: ['style-loader', 'css-loader', 'sass-loader'],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
 };
